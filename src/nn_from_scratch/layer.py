b54211c31fa8483396daf488fd67394b1032bf1a
simo <svm.zhang@gmail.com>
2025-01-17 21:51:43 -0800

    Added ReLU layer class.

--- src/nn_from_scratch/layer.py
+++ src/nn_from_scratch/layer.py
@@ -6,7 +6,7 @@ from scipy import signal
 
 
 class Layer(ABC):
-    def __init__(self):
+    def __init__(self, *kwargs):
         self.input = None
         self.output = None
 
@@ -20,8 +20,12 @@ class Layer(ABC):
 class Dense(Layer):
     def __init__(self, input_size, output_size):
         # self.weights = np.random.randn(output_size, input_size)  # w[j][i]
-        self.bias = np.random.randn(output_size, 1)
+        # self.bias = np.random.randn(output_size, 1)
+        # self.weights = np.random.normal(
+        #     scale=1e-2, size=(output_size, input_size)
+        # )
         self.weights = self._he_init(input_size, output_size)
+        self.bias = np.zeros((output_size, 1))
 
     def _he_init(self, input_size, output_size):
         stddev = np.sqrt(2.0 / input_size)
@@ -32,9 +36,14 @@ class Dense(Layer):
         return np.dot(self.weights, self.input) + self.bias
 
     def backward(self, output_gradient, lr):
+        # print("I am in Dense.backward")
+        # print(f"{output_gradient.shape=}")
+        # print(f"{self.input.shape=}")
+        # print(f"{self.weights.shape=}")
         w_gradient = np.dot(output_gradient, self.input.T)
         self.weights -= lr * w_gradient
         self.bias -= lr * output_gradient
+        # (output_size, input_size) @ (output_size, 1) => (input_size, 1)
         return np.dot(self.weights.T, output_gradient)
 
 
@@ -56,8 +65,10 @@ class Convolution(Layer):
             kernel_size,
         )
         # self.kernels = np.random.rand(*self.kernel_shape)
+        # self.kernels = np.random.normal(scale=1e-2, size=self.kernel_shape)
         self.kernels = self._he_init(*self.kernel_shape)
-        self.biases = np.random.rand(*self.output_shape)
+        self.biases = np.zeros(self.output_shape)
+        # self.biases = np.random.rand(*self.output_shape)
 
     def _he_init(self, depth, input_depth, kernel_height, kernel_width):
         fan_in = input_depth * kernel_height * kernel_width  # Calculate fan-in
@@ -111,4 +122,19 @@ class Reshape(Layer):
     def backward(self, output_gradient, lr):
         return np.reshape(output_gradient, self.input_shape)
 
-    pass
+
+class ReLU(Layer):
+    def __init__(self):
+        pass
+
+    def forward(self, input):
+        self.input = input
+        return np.maximum(0, input)
+
+    def backward(self, output_gradient, lr):
+        relu_grad = (self.input > 0).astype(self.input.dtype)
+        # print("I am in ReLU backward")
+        # print(f"{output_gradient.shape=}")
+        # print(f"{self.input.shape=}")
+        # element-wise multiply
+        return np.multiply(output_gradient, relu_grad)
