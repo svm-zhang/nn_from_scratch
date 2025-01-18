df53bde47fbfa0795b2dfce6422a36120ad98171
simo <svm.zhang@gmail.com>
2025-01-17 21:50:50 -0800

    Implemented cross entropy loss class.

--- src/nn_from_scratch/loss.py
+++ src/nn_from_scratch/loss.py
@@ -24,22 +24,6 @@ def bce_loss_prime(y_true, y_pred):
     return (y_pred - y_true) / (y_pred * (1 - y_pred)) / np.size(y_true)
 
 
-def ce_loss(y_true, y_pred):
-    y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-    print(y_pred)
-    print(y_pred.shape)
-    probs = np.exp(y_pred) / np.sum(np.exp(y_pred))
-    print(probs)
-    print(probs.shape)
-    raise SystemExit
-    loss = -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
-    return loss
-
-
-def ce_loss_prime(y_true, y_pred):
-    pass
-
-
 class Loss(Protocol):
     @staticmethod
     def loss(y_true, y_pred) -> np.float32: ...
@@ -76,18 +60,48 @@ class BCELoss:
 class CELoss:
     @staticmethod
     def loss(y_true, y_pred) -> np.float32:
-        # y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        exp_y_pred = np.exp(y_pred)
-        # probs = exp_y_pred / np.sum(exp_y_pred, axis=1).reshape(-1, 1)
-        probs = exp_y_pred / np.sum(exp_y_pred)
-        print("y_pred_prob", probs)
-        loss = -np.sum(y_true * np.log(probs))
-        # loss = -np.mean(np.sum(y_true * np.log(probs), axis=1))
+        # print(f"{y_true=}")
+        # print(f"{y_pred=} {y_pred.shape=} {len(y_pred)=}")
+        true_class_logits = y_pred[y_true, 0]
+        # print(f"{true_class_logits=}")
+        loss = -true_class_logits + np.log(np.sum(np.exp(y_pred)))
         return loss
 
     @staticmethod
     def loss_prime(y_true, y_pred):
-        exp_y_pred = np.exp(y_pred)
+        ones_true_class = np.zeros_like(y_pred)
+        # print("I am get dl/dz")
+        # print(y_true)
+        # print(f"{ones_true_class=}")
+        ones_true_class[y_true, 0] = 1
+        # print("ones_true_class", ones_true_class)
+        softmax = np.exp(y_pred) / np.exp(y_pred).sum(0, keepdims=True)
+        # print("softmax", softmax, softmax.shape)
+        # print(-ones_true_class + softmax)
+        loss_prime = (-ones_true_class + softmax) / y_pred.shape[0]
+        # print(f"{loss_prime.shape=}")
+        # print("Ending loss_prime")
+        return loss_prime
+
+    @staticmethod
+    def loss_old(y_true, y_pred) -> np.float32:
+        # print("y_true", y_true)
+        # print("y_pred", y_pred)
+        # y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        exp_y_pred = np.exp(y_pred - np.max(y_pred))
+        # probs = exp_y_pred / np.sum(exp_y_pred, axis=1).reshape(-1, 1)
+        probs = exp_y_pred / np.sum(exp_y_pred)
+        if (probs == 0).any():
+            raise SystemExit
+        # print("y_pred_prob", probs)
+        # print("log y_pred_prob", np.log(probs))
+        # print("product y_true log_y_pred_prob", y_true * np.log(probs))
+        loss = -np.sum(np.multiply(y_true, np.log(probs)))
+        return loss
+
+    @staticmethod
+    def loss_prime_old(y_true, y_pred):
+        exp_y_pred = np.exp(y_pred - np.max(y_pred))
         probs = exp_y_pred / np.sum(exp_y_pred)
         loss_prime = probs - y_true
         return loss_prime
