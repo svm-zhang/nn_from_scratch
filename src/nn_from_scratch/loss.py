b887db8cf67e0a3f678fd064d8f312e3a91d1898
simo <svm.zhang@gmail.com>
2025-01-19 23:35:27 -0800

    Implemented cross entropy loss calculation with batching.

--- src/nn_from_scratch/loss.py
+++ src/nn_from_scratch/loss.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import Any, Protocol
 
 import numpy as np
 
@@ -26,10 +26,10 @@ def bce_loss_prime(y_true, y_pred):
 
 class Loss(Protocol):
     @staticmethod
-    def loss(y_true, y_pred) -> np.float32: ...
+    def loss(y_true_idx, logits) -> Any: ...
 
     @staticmethod
-    def loss_prime(y_true, y_pred): ...
+    def loss_prime(y_true_idx, logits) -> Any: ...
 
 
 class MSELoss:
@@ -57,22 +57,45 @@ class BCELoss:
         return (y_pred - y_true) / (y_pred * (1 - y_pred)) / np.size(y_true)
 
 
-class CELoss:
+class CELoss(Loss):
     @staticmethod
-    def loss(y_true, y_pred) -> np.float32:
-        # print(f"{y_true=}")
+    def loss(y_true_idx, logits):
+        # y_true: (b, )
+        # logits: (b, c), c is the number of output classes or output shape
+        # print(f"{logits.shape=} {y_true_idx.shape=}")
+        # print(f"{y_true_idx=}")
+        # true_class_logits: (b, 1)
+        true_class_logits = logits[np.arange(len(logits)), y_true_idx]
+        true_class_logits = true_class_logits.reshape(-1, 1)
+        # print(true_class_logits)
+        # log_denominator: (b, )
+        log_denominator = np.log(np.sum(np.exp(logits), axis=-1))
+        log_denominator = log_denominator.reshape(-1, 1)
+        # print(f"{log_denominator.shape=}")
+        # print(f"{true_class_logits.shape=}")
         # print(f"{y_pred=} {y_pred.shape=} {len(y_pred)=}")
-        true_class_logits = y_pred[y_true, 0]
+        # true_class_logits = y_pred[y_true, 0]
         # print(f"{true_class_logits=}")
-        loss = -true_class_logits + np.log(np.sum(np.exp(y_pred)))
+        loss = -true_class_logits + log_denominator
+        # loss = -true_class_logits + np.log(np.sum(np.exp(logits)))
         return loss
 
     @staticmethod
-    def loss_prime(y_true, y_pred):
-        ones_true_class = np.zeros_like(y_pred)
-        # print("I am get dl/dz")
-        # print(y_true)
-        # print(f"{ones_true_class=}")
+    def loss_prime(y_true_idx, logits) -> np.ndarray:
+        # logits: (b, c)
+        # one_true_class: (b, c)
+        one_true_class = np.zeros_like(logits)
+        # print(f"{one_true_class.shape}")
+        one_true_class[np.arange(len(logits)), y_true_idx] = 1
+        softmax = np.exp(logits) / np.sum(
+            np.exp(logits), axis=-1, keepdims=True
+        )
+        # print(f"{softmax.shape=}")
+        loss_grad = (softmax - one_true_class) / logits.shape[0]
+        # print(f"{loss_grad.shape=}")
+        assert loss_grad.shape == logits.shape
+        return loss_grad
+        # ones_true_class = np.zeros_like(y_pred)
         ones_true_class[y_true, 0] = 1
         # print("ones_true_class", ones_true_class)
         softmax = np.exp(y_pred) / np.exp(y_pred).sum(0, keepdims=True)
@@ -81,27 +104,4 @@ class CELoss:
         loss_prime = (-ones_true_class + softmax) / y_pred.shape[0]
         # print(f"{loss_prime.shape=}")
         # print("Ending loss_prime")
-        return loss_prime
-
-    @staticmethod
-    def loss_old(y_true, y_pred) -> np.float32:
-        # print("y_true", y_true)
-        # print("y_pred", y_pred)
-        # y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        exp_y_pred = np.exp(y_pred - np.max(y_pred))
-        # probs = exp_y_pred / np.sum(exp_y_pred, axis=1).reshape(-1, 1)
-        probs = exp_y_pred / np.sum(exp_y_pred)
-        if (probs == 0).any():
-            raise SystemExit
-        # print("y_pred_prob", probs)
-        # print("log y_pred_prob", np.log(probs))
-        # print("product y_true log_y_pred_prob", y_true * np.log(probs))
-        loss = -np.sum(np.multiply(y_true, np.log(probs)))
-        return loss
-
-    @staticmethod
-    def loss_prime_old(y_true, y_pred):
-        exp_y_pred = np.exp(y_pred - np.max(y_pred))
-        probs = exp_y_pred / np.sum(exp_y_pred)
-        loss_prime = probs - y_true
         return loss_prime
