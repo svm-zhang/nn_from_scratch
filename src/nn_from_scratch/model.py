a36335a343ef0975e45d974100a7abeaae94b39b
simo <svm.zhang@gmail.com>
2025-01-21 14:54:05 -0800

    Added accuracy on val dataset in training loop; Added tqdm support; Cleaned up unused code.

--- src/nn_from_scratch/model.py
+++ src/nn_from_scratch/model.py
@@ -1,35 +1,15 @@
 import numpy as np
 from tqdm.auto import tqdm
 
+from .inference import run_validation
 from .loss import Loss
 
-# TODO::
-# 1. Add validation during training and record accuracy
-# 2. Use tqdm [DONE]
 
-
-def create_batch(x, y, batch_size):
-    n_batch = int(np.ceil(len(x) / batch_size))
-    batch_x = np.array_split(x, n_batch)
-    batch_y = np.array_split(y, n_batch)
-    return batch_x, batch_y
-
-
-# def train(
-#     x_train,
-#     y_train,
-#     network,
-#     loss_fn: Loss,
-#     batch_size: int,
-#     epoches: int = 100,
-#     lr: float = 0.1,
-# ):
 def train(
     train_loader,
-    test_loader,
+    val_loader,
     network,
     loss_fn: Loss,
-    batch_size: int,
     epoches: int = 100,
     lr: float = 0.1,
 ):
@@ -37,7 +17,9 @@ def train(
         tot_epoch_error = 0
         n_train = 0
         batch_iterator = tqdm(train_loader, desc=f"Processing epoch: {e:02d}")
-        for batch_x, batch_y in batch_iterator:
+        for batch in batch_iterator:
+            batch_x = np.stack([train_loader.ds[idx][0] for idx in batch])
+            batch_y = np.stack([train_loader.ds[idx][1] for idx in batch])
             output = batch_x
             for layer in network:
                 output = layer.forward(output)
@@ -51,13 +33,10 @@ def train(
             tot_epoch_error += np.sum(batch_tot_loss)
             ave_batch_loss = np.mean(batch_tot_loss)
             n_train += len(batch_x)
-            batch_iterator.set_postfix({"loss": f"{ave_batch_loss:6.3f}"})
+            batch_iterator.set_postfix({"loss": f"{ave_batch_loss:.3f}"})
         avg_epoch_error = tot_epoch_error / n_train
-        print(f"ave_epoch_err={e+1}/{epoches} {avg_epoch_error=}")
-
-
-def predict(network, input):
-    output = input
-    for layer in network:
-        output = layer.forward(output)
-    return output
+        accuracy = run_validation(val_loader, network)
+        batch_iterator.write(
+            f"ave_epoch_err={e+1}/{epoches} "
+            f"avg_epoch_error={avg_epoch_error:.3f} accuracy={accuracy:.3f}"
+        )
