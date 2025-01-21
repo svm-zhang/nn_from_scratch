5ab6bf1416ae41973b8389517fa9c241b9470110
simo <svm.zhang@gmail.com>
2025-01-21 14:52:02 -0800

    Batching on indices and iterator over indices.

--- src/nn_from_scratch/dataloader.py
+++ src/nn_from_scratch/dataloader.py
@@ -12,17 +12,8 @@ class NaiveDataLoader:
         if self.shuffle:
             np.random.shuffle(self.indices)
 
-        for i in range(0, len(self.ds), self.batch_size):
-            j = min(i + self.batch_size, len(self.ds))
-            batch_indices = self.indices[i:j]
-            yield (
-                np.stack(
-                    [self.ds[idx][0].detach().numpy() for idx in batch_indices]
-                ),
-                np.stack(
-                    [self.ds[idx][1].detach().numpy() for idx in batch_indices]
-                ),
-            )
+        for batch in np.array_split(self.indices, self.__len__()):
+            yield batch
 
     def __len__(self) -> int:
         return len(self.ds) // self.batch_size + 1
