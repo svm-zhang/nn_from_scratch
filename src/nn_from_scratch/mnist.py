2113e6f7907418899cf3aa86f0908707a593e9d3
simo <svm.zhang@gmail.com>
2025-01-21 14:55:53 -0800

    Updated to use dataloader, inference model; Clean out non-related code.

--- src/nn_from_scratch/mnist.py
+++ src/nn_from_scratch/mnist.py
@@ -1,132 +1,45 @@
-from collections import defaultdict
-
-import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-from torchvision.transforms import v2
 
-from .activation import Sigmoid
-from .layer import Convolution, Dense, ReLU, Reshape
-from .loss import BCELoss, CELoss
-from .model import predict, train
-
-
-def mnist():
-    transform = v2.Compose(
-        [
-            v2.PILToTensor(),
-        ]
-    )
-    mnist_data = torchvision.datasets.MNIST(
-        "./mnist/", download=True, train=True, transform=transform
-    )
-    return mnist_data
-
-
-def extract_tensor(dataloader):
-    xs = []
-    ys = []
-    for i, (x, y) in enumerate(dataloader):
-        xs.append(x)
-        ys.append(y)
-    return torch.cat(xs).detach().numpy(), torch.cat(ys).detach().numpy()
-
-
-def preprocess(x, y, size):
-    zero_index = np.where(y == 0)[0][:size]
-    one_index = np.where(y == 1)[0][:size]
-    all_indices = np.hstack((zero_index, one_index))
-    all_indices = np.random.permutation(all_indices)
-    x, y = x[all_indices], y[all_indices]
-    x = x.reshape(len(x), 1, 28, 28)
-    x = x.astype("float32") / 255
-    y = np.eye(2)[y]
-    y = y.reshape(len(y), 2, 1)
-    return x, y
-
-
-def subset(dataset, indices, n_per_label):
-    counter = defaultdict(int)
-    num_classes = len(torch.unique(dataset.targets))
-    sub_xs = []
-    sub_ys = []
-    for idx in indices:
-        x, y = (
-            dataset.data[idx],
-            dataset.targets[idx],
-        )
-        label = y.item()
-        if counter[label] < n_per_label:
-            x = x.float() / 255.0
-            x = torch.reshape(x, (1, x.shape[0], x.shape[1])).detach().numpy()
-            y = F.one_hot(y, num_classes)
-            y = y.view(-1, 1).detach().numpy()
-            sub_xs.append(x)
-            sub_ys.append(y)
-            counter[label] += 1
-
-    sub_x = np.array(sub_xs)
-    sub_y = np.array(sub_ys)
-    return sub_x, sub_y
-
-
-def create_batch(x, y, batch_size):
-    n_batch = int(np.ceil(len(x) / batch_size))
-    batch_x = np.array_split(x, n_batch)
-    batch_y = np.array_split(y, n_batch)
-    return batch_x, batch_y
+from .activation import ReLU
+from .dataloader import NaiveDataLoader
+from .datasets import MNIST
+from .inference import run_validation
+from .layer import BatchNorm1D, Convolution, Dense, Reshape
+from .loss import CELoss
+from .model import train
 
 
 def solve_mnist():
-    mnist_data = mnist()
+    mnist_data = MNIST("./mnist/")
     seed = torch.Generator().manual_seed(2024)
 
-    train_dataset, test_dataset = torch.utils.data.random_split(
-        mnist_data, [0.7, 0.3], generator=seed
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+        mnist_data.data, [0.85, 0.05, 0.1], generator=seed
     )
-    sub_x_train, sub_y_train = subset(mnist_data, train_dataset.indices, 256)
-    sub_x_test, sub_y_test = subset(mnist_data, test_dataset.indices, 32)
-    # batch_x_train, batch_y_train = create_batch(sub_x_train, sub_y_train, 32)
+    batch_size = 128
+    train_loader = NaiveDataLoader(train_dataset, batch_size, shuffle=True)
+    val_loader = NaiveDataLoader(val_dataset, batch_size)
+    test_loader = NaiveDataLoader(test_dataset, batch_size)
 
     n_kernel = 3
     kernel_size = 3
+    fc1 = 128
 
     network = [
         Convolution((1, 28, 28), kernel_size, n_kernel),
-        # Sigmoid(),
         ReLU(),
-        # Convolution((3, 24, 24), kernel_size, 1),
-        Reshape((n_kernel, 26, 26), (n_kernel * 26 * 26, 1)),
-        Dense(n_kernel * 26 * 26, 128),
+        Reshape((n_kernel, 26, 26), n_kernel * 26 * 26),
+        Dense(n_kernel * 26 * 26, fc1),
+        BatchNorm1D(fc1),
         ReLU(),
-        # Sigmoid(),
-        Dense(128, 10),
-        ReLU(),
-        # Sigmoid(),
+        Dense(fc1, 10),
     ]
-    epoch = 120
-    lr = 0.001
+    epoch = 30
+    lr = 0.005
 
-    # loss = BCELoss()
-    loss = CELoss()
+    loss_fn = CELoss()
 
-    train(sub_x_train, sub_y_train, network, loss, epoch, lr)
-    # train(batch_x_train, batch_y_train, cnn, loss, epoch, lr)
+    train(train_loader, val_loader, network, loss_fn, epoch, lr)
 
-    correct = 0
-    i = 0
-    for x, y in zip(sub_x_test, sub_y_test):
-        output = predict(network, x)
-        pred = np.argmax(output)
-        truth = np.argmax(y)
-        if pred == truth:
-            correct += 1
-        else:
-            print(f"{pred=} {truth=}")
-        i += 1
-
-    print(correct)
-    print(len(sub_y_test))
-    print(correct / len(sub_y_test))
+    accuracy = run_validation(test_loader, network)
+    print(f"Test accuracy: {accuracy:.3f}")
