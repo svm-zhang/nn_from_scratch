001822d00a491171fb077c6b37f7b5bf76b87120
simo <svm.zhang@gmail.com>
2025-01-23 11:39:28 -0800

    Updated to use newly implemented model and optimizer class; Moved training loop from train module.

--- src/nn_from_scratch/mnist.py
+++ src/nn_from_scratch/mnist.py
@@ -1,4 +1,6 @@
+import numpy as np
 import torch
+from tqdm.auto import tqdm
 
 from .activation import ReLU
 from .dataloader import NaiveDataLoader
@@ -6,7 +8,9 @@ from .datasets import MNIST
 from .inference import run_validation
 from .layer import BatchNorm1D, Convolution, Dense, Reshape
 from .loss import CELoss
-from .model import train
+from .model import build_cnn_model
+from .optim import SGD
+from .train import train
 
 
 def solve_mnist():
@@ -21,25 +25,60 @@ def solve_mnist():
     val_loader = NaiveDataLoader(val_dataset, batch_size)
     test_loader = NaiveDataLoader(test_dataset, batch_size)
 
-    n_kernel = 3
-    kernel_size = 3
-    fc1 = 128
+    input_shape = (1, 28, 28)
+    output_shape = len(mnist_data.data.classes)
+    epoch = 20
+    lr = 0.001
 
-    network = [
-        Convolution((1, 28, 28), kernel_size, n_kernel),
-        ReLU(),
-        Reshape((n_kernel, 26, 26), n_kernel * 26 * 26),
-        Dense(n_kernel * 26 * 26, fc1),
-        BatchNorm1D(fc1),
-        ReLU(),
-        Dense(fc1, 10),
-    ]
-    epoch = 30
-    lr = 0.005
+    model = build_cnn_model(
+        input_shape,
+        output_shape,
+        [3],
+        [3],
+        [0],
+        [128],
+    )
+    optimizer = SGD(model.parameters(), lr, momentum=0.9)
+
+    # network = [
+    #     Convolution((1, 28, 28), kernel_size, n_kernel),
+    #     ReLU(),
+    #     Reshape((n_kernel, 26, 26), n_kernel * 26 * 26),
+    #     Dense(n_kernel * 26 * 26, fc1),
+    #     BatchNorm1D(fc1),
+    #     ReLU(),
+    #     Dense(fc1, 10),
+    # ]
 
     loss_fn = CELoss()
+    for e in range(epoch):
+        tot_epoch_error = 0
+        n_train = 0
+        batch_iterator = tqdm(train_loader, desc=f"Processing epoch: {e:02d}")
+        for batch in batch_iterator:
+            batch_x = np.stack([train_loader.ds[idx][0] for idx in batch])
+            batch_y = np.stack([train_loader.ds[idx][1] for idx in batch])
+            optimizer.zero_grad()
+            output = model.train(batch_x)
+            batch_y_true_idx = batch_y.argmax(axis=1)
+            loss = loss_fn.loss(batch_y_true_idx, output)
+            grad = loss_fn.loss_prime(batch_y_true_idx, output)
+            model.backward(grad)
+            optimizer.step()
+            assert isinstance(grad, np.ndarray)
+            batch_tot_loss = np.sum(loss, axis=-1)
+            tot_epoch_error += np.sum(batch_tot_loss)
+            ave_batch_loss = np.mean(batch_tot_loss)
+            n_train += len(batch_x)
+            batch_iterator.set_postfix({"loss": f"{ave_batch_loss:.3f}"})
+        avg_epoch_error = tot_epoch_error / n_train
+        accuracy = run_validation(val_loader, model)
+        batch_iterator.write(
+            f"ave_epoch_err={e+1}/{epoch} "
+            f"avg_epoch_error={avg_epoch_error:.3f} accuracy={accuracy:.3f}"
+        )
 
-    train(train_loader, val_loader, network, loss_fn, epoch, lr)
+    # train(train_loader, val_loader, model, optimizer, loss_fn, epoch)
 
-    accuracy = run_validation(test_loader, network)
+    accuracy = run_validation(test_loader, model)
     print(f"Test accuracy: {accuracy:.3f}")
