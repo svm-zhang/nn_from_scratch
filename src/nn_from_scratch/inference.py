da65a711cd1ce6541e67a766af1e367ba11f2b8c
simo <svm.zhang@gmail.com>
2025-01-23 11:32:47 -0800

    Updated to use newly implemented model.

--- src/nn_from_scratch/inference.py
+++ src/nn_from_scratch/inference.py
@@ -3,20 +3,13 @@ import numpy as np
 from .layer import Softmax
 
 
-def predict(network, input):
-    output = input
-    for layer in network:
-        output = layer.forward(output)
-    return output
-
-
-def run_validation(loader, network):
+def run_validation(loader, model):
     softmax = Softmax()
     n_correct, n_test = 0, 0
     for batch in loader:
         batch_x = np.stack([loader.ds[idx][0] for idx in batch])
         batch_y = np.stack([loader.ds[idx][1] for idx in batch])
-        output = predict(network, batch_x)
+        output = model.predict(batch_x)
         probs = softmax.forward(output)
         preds = np.argmax(probs, axis=-1)
         y_true = np.argmax(batch_y, axis=1)
