e5a02f99f07eef2833d8299976b0328fac587ce9
simo <svm.zhang@gmail.com>
2025-01-28 09:40:52 -0800

    Updated to use datasets and dataloader module of this package; cleaned out unused code.

--- src/nn_from_scratch/torch_cnn.py
+++ src/nn_from_scratch/torch_cnn.py
@@ -2,46 +2,13 @@ from collections import defaultdict
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 from torchvision.transforms import v2
+from tqdm.auto import tqdm
 
-
-def mnist():
-    transform = v2.Compose(
-        [
-            v2.PILToTensor(),
-        ]
-    )
-    mnist_data = torchvision.datasets.MNIST(
-        "./mnist/", download=True, train=True, transform=transform
-    )
-    return mnist_data
-
-
-def subset(dataset, indices, n_per_label):
-    counter = defaultdict(int)
-    num_classes = len(torch.unique(dataset.targets))
-    sub_xs = []
-    sub_ys = []
-    for idx in indices:
-        x, y = (
-            dataset.data[idx],
-            dataset.targets[idx],
-        )
-        label = y.item()
-        if counter[label] < n_per_label:
-            x = x.float() / 255.0
-            x = torch.reshape(x, (1, x.shape[0], x.shape[1]))
-            sub_xs.append(x)
-            # y here is not a one-hot encoding, rather it is class index
-            sub_ys.append(y)
-            counter[label] += 1
-
-    sub_x = torch.stack(sub_xs, 0)
-    sub_y = torch.stack(sub_ys)
-    return torch.utils.data.TensorDataset(sub_x, sub_y)
+from .dataloader import NaiveDataLoader
+from .datasets import MNIST
 
 
 class CNNModel(nn.Module):
@@ -61,7 +28,6 @@ class CNNModel(nn.Module):
             ),
             nn.ReLU(),
             nn.Linear(n_neurons, 10),
-            nn.ReLU(),
         )
 
     def forward(self, input):
@@ -69,30 +35,24 @@ class CNNModel(nn.Module):
 
 
 def solve_mnist_with_torch():
-    mnist_data = mnist()
+    mnist_data = MNIST("./mnist/")
     seed = torch.Generator().manual_seed(2024)
-    train_dataset, test_dataset = torch.utils.data.random_split(
-        mnist_data, [0.7, 0.3], generator=seed
+    train_dataset, val_dataset = torch.utils.data.random_split(
+        mnist_data.data["train"], [0.9, 0.1], generator=seed
     )
-
-    n_train_per_label = 2048
-    n_test_per_label = 128
     batch_size = 128
-    train_subset = subset(mnist_data, train_dataset.indices, n_train_per_label)
-    test_subset = subset(mnist_data, test_dataset.indices, n_test_per_label)
-    train_subset_loader = torch.utils.data.DataLoader(
-        train_subset, batch_size=batch_size, shuffle=True
-    )
-    test_subset_loader = torch.utils.data.DataLoader(
-        test_subset, shuffle=False
-    )
+    train_loader = NaiveDataLoader(train_dataset, batch_size, shuffle=True)
+    val_loader = NaiveDataLoader(val_dataset, batch_size)
+    test_loader = NaiveDataLoader(mnist_data.data["test"], batch_size)
+
+    batch_size = 128
 
     epoches = 50
     input_shape = (1, 28, 28)
     kernel_size = 3
-    depth = 8
+    depth = 3
     n_neurons = 128
-    lr = 0.05
+    lr = 0.001
     loss_f = nn.CrossEntropyLoss()
     model = CNNModel(input_shape, kernel_size, depth, n_neurons)
     optimizer = optim.SGD(model.parameters(), lr=lr)
@@ -101,7 +61,7 @@ def solve_mnist_with_torch():
     for epoch in range(epoches):
         epoch_loss = 0.0
         n_batch = 0
-        for x_train, y_train in train_subset_loader:
+        for x_train, y_train in train_loader:
             model.train()
             optimizer.zero_grad()
             logits = model(x_train)
@@ -118,7 +78,7 @@ def solve_mnist_with_torch():
 
     n_correct = 0
     with torch.inference_mode():
-        for x_test, y_test in test_subset_loader:
+        for x_test, y_test in test_loader:
             logits = model(x_test)
             pred = torch.softmax(logits, dim=-1).argmax(dim=-1).squeeze()
             y_test = y_test.squeeze()
@@ -128,5 +88,5 @@ def solve_mnist_with_torch():
             else:
                 print(f"{pred=} {y_test=}")
 
-    acc = n_correct / len(test_subset_loader)
+    acc = n_correct / len(test_loader)
     print(f"{acc=}")
