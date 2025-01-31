c8f04a29971fca48cad96077f984a75c0875e0b0
simo <svm.zhang@gmail.com>
2025-01-31 15:50:50 -0800

    Updated to use the new API model(input) and model.eval().

--- src/nn_from_scratch/inference.py
+++ src/nn_from_scratch/inference.py
@@ -6,10 +6,11 @@ from .layer import Softmax
 def run_validation(loader, model):
     softmax = Softmax()
     n_correct, n_test = 0, 0
+    model.eval()
     for batch in loader:
         batch_x = np.stack([loader.ds[idx][0] for idx in batch])
         batch_y = np.stack([loader.ds[idx][1] for idx in batch])
-        output = model.predict(batch_x)
+        output = model(batch_x)
         probs = softmax.forward(output)
         preds = np.argmax(probs, axis=-1)
         y_true = np.argmax(batch_y, axis=1)
