b71426091fed6f7e3ee694e650288994539658a1
simo <svm.zhang@gmail.com>
2025-02-01 14:45:42 -0800

    Removed xor support.

--- src/nn_from_scratch/nn_from_np.py
+++ src/nn_from_scratch/nn_from_np.py
@@ -1,7 +1,6 @@
 from .cli import parse_cmd
 from .mnist import solve_mnist
 from .torch_cnn import solve_mnist_with_torch
-from .xor import solve_xor
 
 
 def run():
@@ -9,8 +8,6 @@ def run():
 
     args = parser.parse_args()
     match args.dataset:
-        case "xor":
-            solve_xor()
         case "mnist":
             solve_mnist(args)
         case "mnist_torch":
